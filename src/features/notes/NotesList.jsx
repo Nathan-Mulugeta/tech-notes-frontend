d505c353b66e9657fec05f4d44868b8f59a95a8b
Nathan Mulugeta <nathanmulugetacoding@gmail.com>
2023-11-28 23:23:55 +0300

    implement the new user feature

--- src/features/notes/NotesList.jsx
+++ src/features/notes/NotesList.jsx
@@ -15,7 +15,7 @@ const NotesList = () => {
   if (isLoading) content = <p>Loading...</p>;
 
   if (isError) {
-    content = <p className="errmsg">{error?.data?.message}</p>;
+    content = <p className="errmsg">{error?.error}</p>;
   }
 
   if (isSuccess) {
